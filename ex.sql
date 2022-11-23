07ea276606f932fd2a1423f176c36bff3558a533
Kazae67 <Kazae.zaeka@gmail.com>
2022-11-23 11:08:45 +0100

    Exercice 1

--- ex.sql
+++ ex.sql
@@ -1,5 +1,6 @@
-exercice 1
+[1] Nombre de gaulois par lieu (trié par nombre de gaulois décroissant)
 
 SELECT adresse_personnage, COUNT(id_personnage)
 FROM personnage
 GROUP BY id_personnage
+ORDER BY id_personnage DESC
