bb6065edc46b36290548d450dcc21655b1ec13f6
Kazae67 <Kazae.zaeka@gmail.com>
2022-11-23 20:51:28 +0100

    exercice 1

--- ex.sql
+++ ex.sql
@@ -1,6 +1,6 @@
 [1] Nombre de gaulois par lieu (trié par nombre de gaulois décroissant)
 
-SELECT adresse_personnage, COUNT(id_personnage)
+SELECT id_lieu, SUM(id_lieu) 
 FROM personnage
-GROUP BY id_personnage
-ORDER BY id_personnage DESC
+GROUP BY id_lieu
+ORDER BY SUM(id_lieu) DESC
